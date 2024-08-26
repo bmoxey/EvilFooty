93041e0c7c51681763c716ff31eabc62299ec2b3
bmoxey <bmoxey@gmail.com>
2024-08-26 10:41:22 +1000

    Initial Commit

---
+++ EvilFooty/EvilFootyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvilFootyApp.swift
+//  EvilFooty
+//
+//  Created by Brett Moxey on 26/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct EvilFootyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
